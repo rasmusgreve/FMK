856dc8133d2bf22e1286d9bb1da85f35e99e3b42
Rasmus Greve <rasmusgreve@gmail.com>
2012-05-20 16:20:06 +0200

    Changed database to use nas.emilnygaard.dk

--- config.php
+++ config.php
@@ -2,9 +2,10 @@
 
 $databases = array(
     array('localhost','root','','fmk'),
+	array('nas.emilnygaard.com','fmk','fmk','fmk'),
     array('mydb13.surftown.dk','rasmu11_fmk','fmkode','rasmu11_fmk')
 );
-$database = $databases[0];
+$database = $databases[1];
 
 mysql_connect($database[0],$database[1],$database[2]);
 mysql_select_db($database[3]);
